5e01f4581d56c77d8b5f3b648daf31650512feec
Joan Pons <joan.ponssanchis@gmail.com>
2021-04-29 10:50:43 +0200

    Nous canvis sesio 2

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
 $loggedUser = $_SESSION["user"] ?? "";
+    // TODO: Implementar la connexió a la base de dades
+    $pdo = new PDO("mysql:host=mysql-server;dbname=coffee-talks;charset-utf8" , "root" , "secret");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // TODO: Implementar la consulta
+    $stmt = $pdo->prepare("SELECT * FROM article ORDER BY datart DESC");
+    $stmt->execute();
+    
+    //TODO: Esperrem més d'un registre
+    $rows = $stmt->fetchAll();
 
+    var_dump($rows); //Mostrem l'array per comprovar que tenim dades
 ?>
 <html>
 <head>
@@ -14,13 +24,11 @@ $loggedUser = $_SESSION["user"] ?? "";
     <?php else :?>
     <!--TODO: Show posts -->
         <ul>
-            <li>
-                <a href="">Post 1</a> by User 1 from the Category 1
-            </li>
-            <li>
-                <a href="">Post 2</a> by User 2 from the Category 1
-            </li>
-
+            <?php foreach($rows as $row) : ?>
+                <li>
+                    <a href="posts_show.php?id=<?=$row["codart"]?>"><?=$row["titart"]?></a> by User 1 from the Category 1
+                </li>
+            <?php endforeach; ?>
         </ul>
         <p>Clic to <a href="posts_add.php">add</a> a posting.</p>
     <?php endif; ?>
