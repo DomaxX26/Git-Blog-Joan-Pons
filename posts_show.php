cdfb40cc5a9934c9d163a584c4f4b3865e655d9d
Joan Pons <joan.ponssanchis@gmail.com>
2021-05-18 13:54:20 +0200

    Tasca 6

--- posts_show.php
+++ posts_show.php
@@ -41,7 +41,7 @@ if (empty($loggedUser)){
 <h2><?=$article["titart"]?></h2>
 <!--TODO: Si existeix caldrà mostrar les dades obtingudes de la base de dades //-->
 <p><?=$article["bodyart"]?></p>
-<p>Publicat per <strong><?=$article["nomusu"]?></strong> en la categoria <strong><?=$article["nomcat"]?></strong> el <strong><?=$article["datart"]?></strong></p>
+<p>Publicat per <strong><a href='post_by_user.php?id=<?=$article["codusu"]?>'><?=$article["nomusu"]?></a></strong> en la categoria <strong><a href='post_by_category.php?id=<?=$article["codcat"]?>'><?=$article["nomcat"]?></a></strong> el <strong><?=$article["datart"]?></strong></p>
 <p><a href='posts_edit.php'>Edit</a> || <a href='posts_delete.php'>Delete</a> || <a href='comments_add.php'>Add a comment</a></p>
 <hr>
 <?php endif; ?>
