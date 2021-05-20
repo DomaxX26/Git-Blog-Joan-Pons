204524b7294031a459a4329b115871114160cfbd
DomaxX26 <joan.ponssanchis@gmail.com>
2021-05-20 17:48:12 +0200

    Tasca 7.2

--- rss.php
+++ rss.php
@@ -5,7 +5,7 @@ $pdo = new PDO("mysql:host=mysql-server;dbname=coffee-talks;charset-utf8" , "roo
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //Fem la consulta
-$stmt = $pdo->prepare("SELECT * FROM article ORDER BY datart DESC");
+$stmt = $pdo->prepare("SELECT * FROM article ORDER BY datart DESC LIMIT 5");
 $stmt->execute();
 
 $dataset = $stmt-> fetchAll();
@@ -16,23 +16,23 @@ header('Content-type: text/xml; charset="unicode-UTF-8"', true);
 ?>
 
 
-<!---->
+
 <rss version="2.0" >
 <channel>
  <title>Coffee Talks</title>
- <description>This is an example of an RSS feed</description>
+ <description>Este el RSS de la pagina web de Joan</description>
  <link>http://localhost:8080/</link>
  <copyright>Tito Bambino. DomaxX26. xD  </copyright>
- <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>
+ <lastBuildDate><?=date("r")?></lastBuildDate>
  <pubDate><?=date("D, d M Y H:i:s T")?></pubDate>
  <ttl>1800</ttl>
 <?php foreach ($dataset as $data) : ?>
  <item>
-  <title>Example entry</title>
-  <description>Here is some text containing an interesting description.</description>
-  <link>http://www.example.com/blog/post/1</link>
+  <title><?=$data["titart"]?></title>
+  <description><?=$data["bodyart"]?></description>
+  <link>http://localhost:8080/blog/post/1</link>
   <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
-  <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+  <pubDate><?=$data["datart"]?></pubDate>
  </item>
  <?php endforeach; ?>
 </channel>
